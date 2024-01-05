0b01e355fda167beb9366a3cc0b43880a45635bd
CubeConvict <kraft.toby@gmail.com>
2024-01-04 17:26:02 -0800

    Working version of bgg game page scrape and IBM Machine Learning class materials

--- boardgamegeek/scrape_game_page.py
+++ boardgamegeek/scrape_game_page.py
@@ -1,46 +1,43 @@
-from bs4 import BeautifulSoup
-import html5lib
 import requests
-import pandas as pd
 import re
-from lxml import etree
-import re
+import json
 
-nan_value = float("NaN") 
 
 #########################
 def scrape_game_page(this_url):
-    page = requests.get(this_url)
-    html_data = page.text
-    print(type(html_data))
-    max_players = re.search("class=\"ng-binding ng-scope\">–</span><!-- end ngIf: min>0 -->(.[0-9]?)", html_data).group(1)
+    # function for performing a game page scrape and returning the json of the game data
+    page_html = requests.get(this_url)
+    page_text = page_html.text
 
-    print(max_players)
+    pattern = re.compile('GEEK.geekitemPreload =.*')
+
+    game_json_match = pattern.search(page_text)
+
+    json_text = game_json_match.group(0)
+    json_text = json_text.strip("GEEK.geekitemPreload =")
+    json_text = json_text.rstrip(";")
+
+    json_data = json.loads(json_text)
 
 
-    
-    
-    #data.replace("", nan_value, inplace=True) 
-    #data.dropna(how='all', axis=1, inplace=True) 
-    
-    #dataframe has all zeros for index
-    #data.reset_index(inplace=True)
-    return data
+    return json_data
 ###################################################################################
-url = 'https://boardgamegeek.com/boardgame/342942/ark-nova'
-mydata = scrape_game_page(url)
+def get_players(json_data):
+    # function for getting player number data from game json data
+
+    user_recommended_players = json_data['item']['polls']['userplayers']['best']
+    publisher_recommended_players = json_data['item']['polls']['userplayers']['recommended']
+
+    return user_recommended_players, publisher_recommended_players
+
+
+url = 'https://boardgamegeek.com/boardgame/421/1830-railways-robber-barons'
+game_json = scrape_game_page(url)
+list = get_players(game_json)
 
 
 
 
 
-'''
-SAMPLE CODE
-<span ng-if="::geekitemctrl.geekitem.data.item.minplayers > 0 || geekitemctrl.geekitem.data.item.maxplayers > 0"
-    min="::geekitemctrl.geekitem.data.item.minplayers" max="::geekitemctrl.geekitem.data.item.maxplayers"
-    class="ng-scope ng-isolate-scope"> <!-- ngIf: min > 0 --><span ng-if="min > 0"
-        class="ng-binding ng-scope">1</span><!-- end ngIf: min > 0 --><!-- ngIf: max>0 && min != max --><span
-        ng-if="max>0 &amp;&amp; min != max" class="ng-binding ng-scope"><!-- ngIf: min>0 --><span ng-if="min>0"
-            class="ng-binding ng-scope">–</span><!-- end ngIf: min>0 -->4</span><!-- end ngIf: max>0 && min != max -->
-</span>
-'''
+
+
